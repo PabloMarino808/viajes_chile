f905bf0808c21445f3d9a513c4b141f9452bb537
Pablo808 <marinopablo.it@gmail.com>
2024-08-25 17:04:34 -0400

    arreglos visuales finales, orden y documentación, agregado de funciones en javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+    $('[data-toggle="popover"]').popover();
+
+    // El botón de contacto despliega un popover indicando que se envió el mensaje y limpia el contenido del imput
+    $('#btnContacto').click(function(){
+        alert("¡Gracias por tu mensaje, nuestros agentes se pondrán en contacto contigo!");
+        document.getElementById('formNameInput').value = "";
+        document.getElementById('formTitleInput').value = "";
+        document.getElementById('formContentInput').value = "";  
+    });
+
+    // Continua al siguiente elemento del carousel luego de 10 segundos
+    const myCarouselElement = document.querySelector('#carouselHero')
+    const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 10000,
+    })
+
+    // Al hacer scroll por sobre los 56px el color de fondo del navbar cambia de transparente a negro
+    // para que se pueda ver mejor en el resto de la página
     const navEl = document.querySelector('#barranav');
     window.addEventListener('scroll', () => {
 
@@ -10,15 +28,9 @@ $(document).ready(function(){
             navEl.classList.remove('bg-oscuro')
         }
     })
-
-
-    $('.whoarewe').on('click', () => {
-        $('barranav').css(
-            'background-color', 'black'
-        );
-    });
     
-    $(".titulo-tarjeta").on( "click", function() {
-        $( ".contenido-tarjeta").toggle('slow');
+    // Al hacer click la descripción de los elementos de las tarjetas de "quienessomos" aparecen o desaparecen
+    $("#quienessomos").on( "click", function() {
+        $( ".quienContent").toggle('slow');
     });    
 });
